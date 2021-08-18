1afb052f959b4450f8db020a1489937459808931
kaipo <5232643+kaipot@users.noreply.github.com>
2021-08-18 14:52:18 -0700

    replace edit.sh with symlink

--- edit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ -z "$VISUAL" ] && { echo "VISUAL not set" >&2; exit 1; }
-
-FILE="$(dirname "$(realpath "$0")")/keyboards/massdrop/alt/keymaps/kaipot/keymap.c"
-EDITORBIN="$(which "$VISUAL")"
-
-"$EDITORBIN" "$FILE"
-
